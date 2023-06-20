df5e8cb6d34be45ceca4942e5d0419c61541864e
Mete <109483424+meteulken@users.noreply.github.com>
2023-06-20 23:57:29 +0300

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+    int     i = 0;
+    int     rd;
+    char    c;
+    char    *line = malloc(100000);
+    
+    if (BUFFER_SIZE <= 0)
+    {
+        free(line);
+        return (NULL);
+    }
+
+    rd = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1);
+    
+    while (rd > 0)
+    {
+        line[i] = c;
+        i++;
+        if (c == '\n')
+            break ;
+        rd = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1);
+    }
+    
+    line[i] =  '\0';
+    
+    if (rd == -1 || i == 0 || (!line[i - 1] && !rd))
+    {
+        free(line);
+        return (NULL);
+    }
+    return(line);
+}
